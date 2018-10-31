e399a6808ef55b9f84e008694ea11c14fffcef40
pradeepgangwar <pradeepgangwar39@gmail.com>
2018-10-31 21:15:41 +0530

    Make it general

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,9 +1,9 @@
 import Route from "@ember/routing/route";
 import { RouteQueryManager } from "ember-apollo-client";
-import query from "../gql/queries/tasksQuery";
+import query from "../gql/queries/yourQuery";
 
 export default Route.extend(RouteQueryManager, {
   model() {
-    return this.get('apollo').watchQuery({ query }, "tasks");
+    return this.get('apollo').watchQuery({ query }, "yourQuery");
   }
 });
